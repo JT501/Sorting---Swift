e23400d10152bf21c990f58fd4e3d19ed3fe9522
Johnny Tsoi <kiucheongchoi@gmail.com>
2023-11-24 22:17:06 +0800

    New Sorting in Swift

---
+++ Sorting-Swift.playground/Pages/Mergesort.xcplaygroundpage/Contents.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+func mergesort(array: [Int]) -> [Int] {
+    let length = array.count
+    var array = array
+    
+    if length == 1 {
+        return array
+    }
+    
+    let mid = length / 2
+    
+    var left = Array(array[..<mid])
+    var right = Array(array[mid...])
+    
+    left = mergesort(array: left)
+    right = mergesort(array: right)
+    
+    return merge(a: left, b: right)
+}
+
+func merge(a: [Int], b: [Int]) -> [Int] {
+    var result = [Int]()
+    var a = a, b = b
+    
+    while !a.isEmpty && !b.isEmpty {
+        if a.first! <= b.first! {
+            result.append(a.removeFirst())
+        } else {
+            result.append(b.removeFirst())
+        }
+    }
+    
+    result.append(contentsOf: a)
+    result.append(contentsOf: b)
+    
+    return result
+}
+
+var array = [9, 4, 43, 122, 2, 12, 88, 1]
+
+print(mergesort(array: array))
---
+++ Sorting-Swift.playground/Pages/Quicksort.xcplaygroundpage/Contents.swift
@@ -0,0 +1,52 @@
+func swap(array: inout [Int], index1: Int, index2: Int) {
+    var temp: Int
+    
+    temp = array[index1]
+    array[index1] = array[index2]
+    array[index2] = temp
+}
+
+func partition(array: inout [Int], left: Int, right: Int) -> Int {
+    var pivot = array[(left + right) / 2]
+    var l = left
+    var r = right
+    
+    while l <= r {
+        while array[l] < pivot {
+            l += 1
+        }
+        
+        while array[r] > pivot {
+            r -= 1
+        }
+        
+        if l <= r {
+            swap(array: &array, index1: l, index2: r)
+            l += 1
+            r -= 1
+        }
+    }
+    
+    return l
+}
+
+func quicksort(array: inout [Int], leftIndex: Int, rightIndex: Int) {
+    var pivotIndex: Int
+    
+    if array.count > 1 {
+        pivotIndex = partition(array: &array, left: leftIndex, right: rightIndex)
+        
+        if leftIndex < pivotIndex - 1 {
+            quicksort(array: &array, leftIndex: leftIndex, rightIndex: pivotIndex - 1)
+        }
+        
+        if rightIndex > pivotIndex {
+            quicksort(array: &array, leftIndex: pivotIndex, rightIndex: rightIndex)
+        }
+    }
+}
+
+var unsort = [19, 22, 63, 105, 2, 46]
+
+quicksort(array: &unsort, leftIndex: 0, rightIndex: unsort.count - 1)
+print(unsort)
